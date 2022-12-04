35f6ab54aa2cee26394718c3c7ae319d16ec9d8f
elllizaveta <111683005+elllizaveta@users.noreply.github.com>
2022-12-05 00:50:48 +0300

    Add files via upload

---
+++ Globals.py
@@ -0,0 +1,11 @@
+class Globals:
+    def __init__(self):
+        self.player_name
+        self.default_player_name
+
+        self.SCREEN = WIDTH, HEIGHT = 300, 480
+        self.SIZE = 20
+        self.ROWS = (HEIGHT - 120) // self.SIZE
+        self.COLS = WIDTH // self.SIZE
+
+Globals = Globals()
